1218e22a6bdda65602cc713677aafdb8ca612542
Kota002 <kotanitaketo@gmail.com>
2025-05-28 14:52:30 +0900

    現在動いている状態を保存

---
+++ info/contact.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="ja">
+<?php
+// 入力データを取得&バリデーション
+$name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+$name_furigana = htmlspecialchars($_POST['name_furigana'], ENT_QUOTES, 'UTF-8');
+$tel = htmlspecialchars($_POST['tel'], ENT_QUOTES, 'UTF-8');
+$timezone = htmlspecialchars($_POST['timezone'], ENT_QUOTES, 'UTF-8');
+$message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
+$subject = htmlspecialchars($_POST['subject'], ENT_QUOTES, 'UTF-8');
+//メールアドレス内に不要な改行があった場合それを消す
+$email = str_replace(array("\r", "\n"), '', $_POST['email']);
+// メール内容
+$to = 'terabochi@terabochi.com';  // 送信先メールアドレス
+$body = "お名前: $name\nフリガナ: $name_furigana\nメールアドレス: $email\n電話番号:$tel\n連絡希望時間:$timezone\nお問い合わせ内容:\n$message";
+// ヘッダー（送信元メールアドレスなど）
+$headers = "From: " . $email;
+?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=yes, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="description" content="寺墓地.COM(ドットコム) - 新星会合同会社のサイトです">
+  <title>寺墓地.COM(ドットコム)</title>
+  <link rel="stylesheet" href="../style.css" />
+</head>
+
+<body>
+  <!--固定ヘッダー部分の作成-->
+  <header>
+    <div class="header_inner">
+      <div class="header_inner_top_box">
+        <p class="header_inner_logo">
+          <!--タイトルロゴの位置-->
+          <a href="../_index.html"><img src="../images/top/top_titlerogo2.svg" alt="タイトルロゴ"
+              class="header_title_logo" width="265" height="85"></a>
+          <a href="../info\index.html"><img src="../images/top/footer_tel.png" alt="電話番号:0120-686-684"
+              class="info" width="265" height="80"></a>
+        </p>
+      </div>
+      <!--固定ヘッダー内のメニューボタン-->
+      <div class="header_inner_menu">
+        <a href="../_index.html"><img src="../images/top/UI_top_search.svg" alt="寺墓地を探す" class="UI_top_funeral"
+            width="200" height="150"></a>
+        <a href="../_index.html"><img src="../images/top/UI_top_eidaikuyou.svg" alt="永代供養墓"
+            class="UI_top_funeral" width="200" height="70"></a>
+        <a href="../_index.html"><img src="../images/top/UI_top_funeral.svg" alt="葬儀" class="UI_top_funeral"
+            width="200" height="70"></a>
+        <a href="../_index.html"><img src="../images/top/UI_top_moving.svg" alt="お墓の引っ越し" class="UI_top_funeral"
+            width="200" height="70"></a>
+        <a href="../yombstone/index.html"><img src="../images/top/UI_top_tombstone.svg" alt="墓石"
+            class="UI_top_funeral" width="200" height="70"></a>
+      </div>
+    </div>
+  </header>
+
+  <!--コンテンツ部分-->
+  <!-- ここにトップページの本文を追加していく-->
+  <main>
+    <!-- お問い合わせフォーム -->
+    <div class="info_contact_inner">
+      <h2>
+        <div class="info_main_inner_header_text">メール内容確認</div>
+      </h2>
+      <!-- お問い合わせ内容のインプット -->
+      <div class="from_contact">
+        <!--メール送信用フォーム-->
+        <form method="POST" action="mail.php">
+          <!--フォームの内容を表示する-->
+          <div class="from-Item">
+            <p class="from-Item-Label">ご相談内容</p>
+            <div class="from-Item-element"><?php echo $subject; ?></div>
+            <p class="from-Item-Label"><span class="from-Item-Label-Required">必須</span>お名前</p>
+            <div class="from-Item-element"><?php echo $name; ?></div>
+            <p class="from-Item-Label"><span class="from-Item-Label-Required">必須</span>フリガナ</p>
+            <div class="from-Item-element"><?php echo $name_furigana; ?></div>
+            <p class="from-Item-Label"><span class="from-Item-Label-Required">必須</span>ご連絡先電話番号</p>
+            <div class="from-Item-element"><?php echo $tel; ?></div>
+            <p class="from-Item-Label">メールアドレス</p>
+            <div class="from-Item-element"><?php echo $email; ?></div>
+            <p class="from-Item-Label">ご連絡希望時間帯</p>
+            <div class="from-Item-element" name="timezone"><?php echo $timezone; ?></div>
+            <p class="from-Item-Label isMsg">お問い合わせ内容</p>
+            <div class="from-Item-element" id="message" name="message"><?php echo $message; ?></div>
+          </div>
+          
+          <!-- hiddenで値を次の画面に渡す -->
+          <input type="hidden" name="subject" value="<?= $subject ?>">
+          <input type="hidden" name="name" value="<?= $name ?>">
+          <input type="hidden" name="name_furigana" value="<?= $name_furigana ?>">
+          <input type="hidden" name="tel" value="<?= $tel ?>">
+          <input type="hidden" name="email" value="<?= $email ?>">
+          <input type="hidden" name="timezone" value="<?= $timezone ?>">
+          <input type="hidden" name="message" value="<?= $message ?>">
+
+          <div class="from-Item-button">
+            <input type="submit" class="from-Btn" value="送信する">
+            <button type="button" class="from-Btn" onclick="history.back()">戻る</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <!--フッター情報欄-->
+    <footer class="footer">
+      <div class="footer_banner">
+        <a href="https://www.tojyuin.com/"><img src="../images/footer/bn_01.jpg" alt="東樹院"
+            class="footer_banner_image" width="222*" height="144"></a>
+        <a href="http://www.project-x.co.jp/320003/index.shtml"><img src="../images/footer/bn_02.jpg"
+            alt="昭和セレモニー" class="footer_banner_image" width="222" height="144"></a>
+        <a href="https://little-emperor.weebly.com/"><img src="../images/footer/bn_03.png" alt="小皇帝合同会社"
+            class="footer_banner_image" width="222" height="144"></a>
+      </div>
+      <div class="footer_text">
+        <p>Copyright © 2015 新星会合同会社 All Rights Reserved designed</p>
+      </div>
+    </footer>
+  </main>
+
+  <style>
+    main {
+      width: 100%;
+      height: 100%;
+      box-sizing: border-box;
+    }
+
+    .info_contact_inner {
+      margin-left: 45%;
+      box-sizing: border-box;
+    }
+
+    .info_main_inner_header_text {
+      box-sizing: border-box;
+      padding-left: 50%;
+      box-sizing: border-box;
+    }
+
+    .from-Item {
+      padding-right: 610px;
+      box-sizing: border-box;
+    }
+
+    .from-Item-button {
+      padding-left: 175px;
+      box-sizing: border-box;
+    }
+
+    .from-Item-Label {
+      margin-bottom: auto;
+      margin-right: 500px;
+      box-sizing: border-box;
+    }
+
+    .from-Item-element {
+      margin-left: 30px;
+      padding: 10px;
+      box-sizing: border-box;
+      background-color: rgba(255, 255, 255, 0.35);
+      border-radius: 6px;
+    }
+
+    .from_contact {
+      width: 100%;
+      height: 100%;
+      margin-left: 10%;
+      box-sizing: border-box;
+    }
+  </style>
+
+  /*
+  if (mb_send_mail($to, $subject, $body, $headers)) {
+  echo "メールを送信しました。ありがとうございました。";
+  } else {
+  echo "HTMLからのPOST送信受信に失敗しました";
+  }*/
---
+++ info/mail.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="ja">
+<?php
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+// 入力データを取得&バリデーション
+$name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+$name_furigana = htmlspecialchars($_POST['name_furigana'], ENT_QUOTES, 'UTF-8');
+$tel = htmlspecialchars($_POST['tel'], ENT_QUOTES, 'UTF-8');
+$timezone = htmlspecialchars($_POST['timezone'], ENT_QUOTES, 'UTF-8');
+$message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
+$subject = htmlspecialchars($_POST['subject'], ENT_QUOTES, 'UTF-8');
+//メールアドレス内に不要な改行があった場合それを消す
+$email = str_replace(array("\r", "\n"), '', $_POST['email']);
+// メール内容
+$to = 'terabochi@terabochi.com';  // 送信先メールアドレス
+$body = "お名前: $name\nフリガナ: $name_furigana\nメールアドレス: $email\n電話番号:$tel\n連絡希望時間:$timezone\nお問い合わせ内容:\n$message";
+// ヘッダー（送信元メールアドレスなど）
+$headers = "From: " . $email;
+
+$success = mb_send_mail($to, $subject, $body, $headers);
+
+$success_text = "";
+
+if ($success) {
+  $success_text = "メールの送信が完了しました。";
+} else {
+  $success_text = "メールの送信に失敗しました。\nご不明な点やご質問がございましたら、お手数ではございますが、0120-686-684までご連絡お願いします。";
+}
+?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=yes, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="description" content="寺墓地.COM(ドットコム) - 新星会合同会社のサイトです">
+  <title>寺墓地.COM(ドットコム)</title>
+  <link rel="stylesheet" href="../style.css" />
+</head>
+
+<body>
+  <!--固定ヘッダー部分の作成-->
+  <header>
+    <div class="header_inner">
+      <div class="header_inner_top_box">
+        <p class="header_inner_logo">
+          <!--タイトルロゴの位置-->
+          <a href="../_index.html"><img src="../images/top/top_titlerogo2.svg" alt="タイトルロゴ"
+              class="header_title_logo" width="265" height="85"></a>
+          <a href="../info\index.html"><img src="../images/top/footer_tel.png" alt="電話番号:0120-686-684"
+              class="info" width="265" height="80"></a>
+        </p>
+      </div>
+      <!--固定ヘッダー内のメニューボタン-->
+      <div class="header_inner_menu">
+        <a href="../_index.html"><img src="../images/top/UI_top_search.svg" alt="寺墓地を探す" class="UI_top_funeral"
+            width="200" height="150"></a>
+        <a href="../_index.html"><img src="../images/top/UI_top_eidaikuyou.svg" alt="永代供養墓"
+            class="UI_top_funeral" width="200" height="70"></a>
+        <a href="../_index.html"><img src="../images/top/UI_top_funeral.svg" alt="葬儀" class="UI_top_funeral"
+            width="200" height="70"></a>
+        <a href="../_index.html"><img src="../images/top/UI_top_moving.svg" alt="お墓の引っ越し" class="UI_top_funeral"
+            width="200" height="70"></a>
+        <a href="../yombstone/index.html"><img src="../images/top/UI_top_tombstone.svg" alt="墓石"
+            class="UI_top_funeral" width="200" height="70"></a>
+      </div>
+    </div>
+  </header>
+
+  <!--コンテンツ部分-->
+  <!-- ここにトップページの本文を追加していく-->
+  <main>
+    <!-- お問い合わせフォーム -->
+    <div class="info_contact_inner">
+      <!-- お問い合わせ内容のインプット -->
+      <div class="from_contact">
+        <!--メール送信用フォーム-->
+          <div class="from-Item">
+              <h3><?php echo $success_text; ?></h3>
+          </div>
+          <div class="from-Item-button">
+            <button onclick="location.href='../../'" class="from-Btn">トップページに戻る</button>
+          </div>
+      </div>
+    </div>
+
+    <!--フッター情報欄-->
+    <footer class="footer">
+      <div class="footer_banner">
+        <a href="https://www.tojyuin.com/"><img src="../images/footer/bn_01.jpg" alt="東樹院"
+            class="footer_banner_image" width="222*" height="144"></a>
+        <a href="http://www.project-x.co.jp/320003/index.shtml"><img src="../images/footer/bn_02.jpg"
+            alt="昭和セレモニー" class="footer_banner_image" width="222" height="144"></a>
+        <a href="https://little-emperor.weebly.com/"><img src="../images/footer/bn_03.png" alt="小皇帝合同会社"
+            class="footer_banner_image" width="222" height="144"></a>
+      </div>
+      <div class="footer_text">
+        <p>Copyright © 2015 新星会合同会社 All Rights Reserved designed</p>
+      </div>
+    </footer>
+  </main>
+
+  <style>
+    main {
+      width: 100%;
+      height: 100%;
+      box-sizing: border-box;
+    }
+    .from_contact
+    {
+      margin-left: 65%;
+    }
+    .info_contact_inner {
+      box-sizing: border-box;
+    }
+
+    .info_main_inner_header_text {
+      box-sizing: border-box;
+    }
+
+    .from-Item {
+      text-align: center;
+      box-sizing: border-box;
+    }
+
+    .from-Item-button {
+      box-sizing: border-box;
+    }
+
+    .from-Item-Label {
+      margin-right: 500px;
+      box-sizing:border-box;
+    }
+    h3
+    {
+      font-size: 25px;
+      box-sizing:border-box;
+    }
+  </style>
+
+  /*
+  if (mb_send_mail($to, $subject, $body, $headers)) {
+  echo "メールを送信しました。ありがとうございました。";
+  } else {
+  echo "HTMLからのPOST送信受信に失敗しました";
+  }*/
